82036903d8d4ce32ad06e3a4f12c6c6c8a6ac3c6
Pol Sorigue <108137110+psorigue@users.noreply.github.com>
2025-04-16 09:27:28 +0100

    Refactor scripts for BayesTraits analysis: improve variable quoting, streamline file handling, and enhance readability

--- Scripts/14.2.BayesTraits_run.sh
+++ Scripts/14.2.BayesTraits_run.sh
@@ -11,35 +11,34 @@ folder_trees="${folder_BT}/trees" # Where trees are stored. Basename of the nexu
 folder_Lh="${folder_BT}/Lh" # Where likelihood files are stored. Should be inside $folder_BT
 folder_parameters_files="${folder_BT}/param_files" # Where parameter files are stored. Should be inside $folder_BT
 
-
-cd $folder_datasets # It needs to be run in the folder where BayesTraits is installed
+cd "$folder_datasets" # It needs to be run in the folder where BayesTraits is installed
 
 for file in *.txt ; do # Do it for all files in folder
     
     # Get the name of the file without the extension
-    name=$( echo $file | sed "s/.txt//g" ) 
+    name=$( echo "$file" | sed "s/.txt//g" ) 
 
     # Create two copies of the file. One for dependent and one for independent models
-    cp $file "$name"_ind.txt ; mv $file "$name"_dep.txt
+    cp "$file" "${name}_ind.txt" ; mv "$file" "${name}_dep.txt"
 
-    cd $folder_BT
+    cd "$folder_BT"
 
     # Run BayesTraits
     date
 
     # Run both models
-    ./BayesTraitsV4 "$folder_trees"/"$name".nex $folder_datasets/"$name"_dep.txt < $folder_parameters_files/dep_it7.txt > $folder_datasets/"$name"_dep.txt
-    ./BayesTraitsV4 "$folder_trees"/"$name".nex $folder_datasets/"$name"_ind.txt < $folder_parameters_files/ind_it7.txt > $folder_datasets/"$name"_ind.txt
+    ./BayesTraitsV4 "${folder_trees}/${name}.nex" "${folder_datasets}/${name}_dep.txt" < "${folder_parameters_files}/dep_it7.txt" > "${folder_datasets}/${name}_dep.txt"
+    ./BayesTraitsV4 "${folder_trees}/${name}.nex" "${folder_datasets}/${name}_ind.txt" < "${folder_parameters_files}/ind_it7.txt" > "${folder_datasets}/${name}_ind.txt"
     
     # Stones file to obtain marginal likelihood values
-    indep=$( grep 'marginal' $folder_datasets/"$name"_dep*Stones.txt | cut -f2 ) ; dep=$( grep 'marginal' $folder_datasets/"$name"_ind*Stones.txt | cut -f2 )
+    indep=$( grep 'marginal' "${folder_datasets}/${name}_dep"*Stones.txt | cut -f2 ) ; dep=$( grep 'marginal' "${folder_datasets}/${name}_ind"*Stones.txt | cut -f2 )
     
     # Output the likelihood values into Lh folder
-    echo -e "$name\tLh_indep: $indep\tLh_dep: $dep" > "$folder_Lh"/"$name"_Lh.txt
+    echo -e "${name}\tLh_indep: ${indep}\tLh_dep: ${dep}" > "${folder_Lh}/${name}_Lh.txt"
             
     date
 
-    cd $folder_datasets # Retrun to the datasets folder
+    cd "$folder_datasets" # Return to the datasets folder
 
 done
 
--- Scripts/14.5.BT_reverse-jump.sh
+++ Scripts/14.5.BT_reverse-jump.sh
@@ -9,38 +9,36 @@ tree="Data/14.BayesTraits/in/reverse-jump/tree_pb_sc.nex" # Tree file to be used
 
 # Paths
 folder_BT="Local/path/to/BayesTraits" # Path where BayesTraitsV4 is installed
-# Path reverse-jump
 path_rj="${folder_BT}/folder_out/" # Path to run the reverse-jump
 
-
 # All files and folders should be inside the folder where BayesTraits is installed
-cp $dataset $path_rj/dataset.txt ; cp $tree $path_rj/tree.nex # Copy dataset and tree to the folder where BayesTraits is installed
+cp "$dataset" "$path_rj/dataset.txt" ; cp "$tree" "$path_rj/tree.nex" # Copy dataset and tree to the folder where BayesTraits is installed
 
 # Follwing developer's advice, we run 3 times for dependent model and 3 times for independent model.
 # Make folders for all runs
 cd "$path_rj" ; mkdir ML Dep1 Dep2 Dep3 Ind1 Ind2 Ind3
         
 # Copy dataset in all folders
-for dir in */ ; do cp dataset.txt "$dir"/dataset.txt ; done
+for dir in */ ; do cp dataset.txt "$dir/dataset.txt" ; done
 
 # Run ML model
 cd ML/ ; cp dataset.txt dataset_dep.txt ; mv dataset.txt dataset_ind.txt
 
-cd $folder_BT        
-tree="$path_rj"/"$sit"/tree.nex
-dataset_dep="$path_rj"/ML/dataset_dep.txt ; dataset_ind="$path_rj"/ML/dataset_ind.txt
+cd "$folder_BT"        
+tree="$path_rj/$sit/tree.nex"
+dataset_dep="$path_rj/ML/dataset_dep.txt" ; dataset_ind="$path_rj/ML/dataset_ind.txt"
 
-./BayesTraitsV4 $tree $dataset_dep < ./param_files/param_rj_ML_dep.txt
-./BayesTraitsV4 $tree $dataset_ind < ./param_files/param_rj_ML_ind.txt
+./BayesTraitsV4 "$tree" "$dataset_dep" < ./param_files/param_rj_ML_dep.txt
+./BayesTraitsV4 "$tree" "$dataset_ind" < ./param_files/param_rj_ML_ind.txt
 
 # Run MCMC models
 for i in {1..3} ; do
 
-    dataset_dep="$path_rj"/Dep"$i"/dataset.txt ; dataset_ind="$path_rj"/Ind"$i"/dataset.txt
+    dataset_dep="$path_rj/Dep$i/dataset.txt" ; dataset_ind="$path_rj/Ind$i/dataset.txt"
     
     # Reverse-jump with exponential prior
-    ./BayesTraitsV4 $tree $dataset_dep < ./param_files/param_rj_exp_dep.txt # Dependent
-    ./BayesTraitsV4 $tree $dataset_ind < ./param_files/param_rj_exp_ind.txt # Independent
+    ./BayesTraitsV4 "$tree" "$dataset_dep" < ./param_files/param_rj_exp_dep.txt # Dependent
+    ./BayesTraitsV4 "$tree" "$dataset_ind" < ./param_files/param_rj_exp_ind.txt # Independent
     # These commands create output files in the folder where the initial dataset was located. 
 
 done
